513c7dc9bf0913a9bb9863a429ab6502c3d36e3f
tihuan <TimmyHuang0404@Gmail.com>
2014-09-16 20:21:11 -0700

    don't initialize @values

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_accessor :values
+
+  def roll(rolls)
+    @values = []
+    rolls.times { values << (1 + rand(6)) }
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -39,6 +44,7 @@ class AboutDiceProject < Neo::Koan
 
     dice.roll(5)
     second_time = dice.values
+    p "first_time: #{first_time}, second: #{second_time}"
 
     assert_not_equal first_time, second_time,
       "Two rolls should not be equal"
